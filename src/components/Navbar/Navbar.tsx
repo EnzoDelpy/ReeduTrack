7a286da7f70055c0b1144f91eed6ab48ed88e68f
EnzoDelpy <enzodelpy62@gmail.com>
2024-10-07 22:22:52 +0200

    added Navitem animations and nav selection

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,42 @@
-import Navitem from './Navitem.tsx'
-import logo from './../../assets/images/logo.png'
+import Navitem from "./Navitem.tsx";
+import logo from "./../../assets/images/logo.png";
+import HomeIcon from "../icons/HomeIcon.tsx";
+import UserIcon from "../icons/UserIcon.tsx";
+import LogOutIcon from "../icons/LogoutIcon.tsx";
+import { useState } from "react";
 
-export default function Navbar(){
-    return <div className="h-[calc(100svh_-_1.5rem)] w-[22.5rem] bg-white m-3 rounded-lg shadow-[rgba(0,_0,_0,_0.1)_-4px_9px_25px_-6px] p-3 flex flex-col gap-4">
-        <div className="flex mb-4 items-center gap-3">
-            <img src={logo} alt="ReeduTrack logo"  className="w-9 h-9"/>
-            <h2 className="font-bold text-2xl">ReeduTrack</h2>
-        </div>
-        <Navitem>Accueil</Navitem>
+export default function Navbar() {
+  const [activeTab, setActiveTab] = useState<string>("home");
+
+  return (
+    <div className="h-[calc(100svh_-_1.5rem)] w-[22.5rem] bg-white m-3 rounded-lg shadow-[rgba(0,_0,_0,_0.1)_-4px_9px_25px_-6px] p-3 flex flex-col gap-6">
+      <div className="flex items-center gap-3">
+        <img src={logo} alt="ReeduTrack logo" className="w-9 h-9" />
+        <h2 className="font-bold text-2xl">ReeduTrack</h2>
+      </div>
+      <ul className="flex flex-col gap-4 h-full [&>*:last-child]:mt-auto">
+        <Navitem
+          currentPage={activeTab}
+          setActiveTab={setActiveTab}
+          page="home"
+        >
+          <HomeIcon></HomeIcon>Accueil
+        </Navitem>
+        <Navitem
+          currentPage={activeTab}
+          setActiveTab={setActiveTab}
+          page="profile"
+        >
+          <UserIcon></UserIcon>Profil
+        </Navitem>
+        <Navitem
+          currentPage={activeTab}
+          setActiveTab={setActiveTab}
+          page="logout"
+        >
+          <LogOutIcon></LogOutIcon>Se déconnecter
+        </Navitem>
+      </ul>
     </div>
+  );
 }
--- src/components/Navbar/Navitem.tsx
+++ src/components/Navbar/Navitem.tsx
@@ -1,13 +1,27 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
 interface Props {
-    children: ReactNode
+  children: ReactNode;
+  currentPage: string;
+  setActiveTab: (tabName: string) => void;
+  page: string;
 }
 
-export default function Navitem({children} : Props){
-    return(
-        <div className="flex items-center rounded-lg bg-bg-color w-full h-12 gap-3 px-4">
-            {children}
-        </div>
-    )
+export default function Navitem({
+  children,
+  currentPage,
+  setActiveTab,
+  page,
+}: Props) {
+  return (
+    <li
+      className={
+        "flex items-center rounded-lg w-full h-12 gap-3 px-4 hover:bg-bg-color transition-all cursor-pointer " +
+        (currentPage == page ? "bg-bg-color font-semibold" : "")
+      }
+      onClick={() => setActiveTab(page)}
+    >
+      {children}
+    </li>
+  );
 }
