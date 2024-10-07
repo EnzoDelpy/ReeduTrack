ae12053fb2dad5e0e2712bc68e9ac7717eca9915
EnzoDelpy <enzodelpy62@gmail.com>
2024-10-07 22:50:29 +0200

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.tsx";
+import Home from "./components/Home/Home.tsx";
+import Profile from "./components/Profile/Profile.tsx";
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
+      <BrowserRouter>
+        <header>
+          <Navbar></Navbar>
+        </header>
+        <main className="ml-[23.25rem] px-8 py-20">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Routes>
+        </main>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Navbar/Navitem.tsx
+++ src/components/Navbar/Navitem.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from "react";
+import { Link } from "react-router-dom";
 
 interface Props {
   children: ReactNode;
   currentPage: string;
   setActiveTab: (tabName: string) => void;
   page: string;
+  route: string;
 }
 
 export default function Navitem({
@@ -12,16 +14,19 @@ export default function Navitem({
   currentPage,
   setActiveTab,
   page,
+  route,
 }: Props) {
   return (
-    <li
-      className={
-        "flex items-center rounded-lg w-full h-12 gap-3 px-4 hover:bg-bg-color transition-all cursor-pointer " +
-        (currentPage == page ? "bg-bg-color font-semibold" : "")
-      }
-      onClick={() => setActiveTab(page)}
-    >
-      {children}
+    <li onClick={() => setActiveTab(page)}>
+      <Link
+        to={route}
+        className={
+          "flex items-center rounded-lg w-full h-12 gap-3 px-4 hover:bg-bg-color transition-all cursor-pointer " +
+          (currentPage == page ? "bg-bg-color font-semibold" : "")
+        }
+      >
+        {children}
+      </Link>
     </li>
   );
 }
